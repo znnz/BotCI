53ea7048a1de3ce1309b0b21eebab81c053ac9c2
nyizin <kydonnyi@Kydons-MacBook-Pro.local>
2017-08-31 16:34:54 +0800

    initial commit

---
+++ BotCITests/BotCITests.swift
@@ -0,0 +1,36 @@
+//
+//  BotCITests.swift
+//  BotCITests
+//
+//  Created by Kydon Nyi on 31/8/17.
+//  Copyright © 2017 Kydon Nyi. All rights reserved.
+//
+
+import XCTest
+@testable import BotCI
+
+class BotCITests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
